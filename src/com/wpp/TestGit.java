08cb7a5f556e8b3bf6bc74ce03320c1969deaa7f
wupengpeng <w2471485070@outlook.com>
2020-09-03 10:11:28 +0800

    加强了一下hello world

--- src/com/wpp/TestGit.java
+++ src/com/wpp/TestGit.java
@@ -3,5 +3,6 @@ package com.wpp;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
